a6cd52c205901252d2e8df6d22d144e903de5f02
iamsirah <blagolewe@gmail.com>
2023-07-18 06:24:25 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Prints out _putchar using printf
+ * Followed by a new line
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This readme file is all about programming with nested loops in C
