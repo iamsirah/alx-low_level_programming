6c013b189ccdbda25d3e0e8cbf7dad2934c17b81
iamsirah <blagolewe@gmail.com>
2023-07-20 10:57:42 -0700

    Nested loop

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer
+ * @n: is the integer to print
+ */
+
+void	print_number(int n)
+{
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+	if (number > 9)
+	{
+		print_number(number / 10);
+	}
+	_putchar(number % 10 + '0');
+}
