85df999477f2af7ef757e48f0f0c0900ddaf0469
iamsirah <blagolewe@gmail.com>
2023-07-22 16:20:31 +0000

    Print line in terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line in terminal
+ * @n: number of times the character should be printed
+ * Return: 0 for success
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+
+	int i;
+
+	for (i = 0; i <= n; i++)
+
+	{
+		_putchar('-');
+	}
+	{
+		_putchar('\n');
+	}
+	}
+}
+
+
