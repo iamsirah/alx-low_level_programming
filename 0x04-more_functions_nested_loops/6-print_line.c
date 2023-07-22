c74c0cd27abd21449222490b7d70f025ad0c70ec
iamsirah <blagolewe@gmail.com>
2023-07-22 16:52:52 +0000

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,20 +10,19 @@ void print_line(int n)
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
 
 	int i;
 
 	for (i = 0; i <= n; i++)
 
-	{
+		{
 		_putchar('-');
-	}
-	{
+		}
+		{
 		_putchar('\n');
-	}
+		}
 	}
 }
 
